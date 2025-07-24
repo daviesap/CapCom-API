19a0fbe9a8dfc918c82f1a3bfab1d5de578a695b
daviesap <77567470+daviesap@users.noreply.github.com>
2025-07-24 16:35:43 +0100

    Minor tweaks

--- functions/utils/cleanJSON.mjs
+++ functions/utils/cleanJSON.mjs
@@ -3,6 +3,7 @@ export function cleanJson(dirtyString) {
     const cleanedString = dirtyString
       .replace(/\r?\n/g, ' ')     // Replace actual line breaks with space
       .replace(/\\n/g, ' ')       // Replace escaped \n with space
+      // eslint-disable-next-line no-control-regex
       .replace(/[\u0000-\u001F]+/g, ''); // Remove control chars (e.g., tabs, backspace)
   
     try {
